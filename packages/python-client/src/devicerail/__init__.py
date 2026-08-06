c920870450f705ee8683fd31da2dbe6b6969eb6d
Wayne <dengfengwong@gmail.com>
2026-08-06 23:50:38 +0800

    release: v0.3.5

--- packages/python-client/src/devicerail/__init__.py
+++ packages/python-client/src/devicerail/__init__.py
@@ -17,7 +17,7 @@ from .errors import (
 from .protocol.v1 import METHOD_SPECS, RpcMethod, RpcMethodMap, StdioRpcMethod
 from .types import RequestHandle
 
-__version__ = "0.3.4"
+__version__ = "0.3.5"
 
 __all__ = [
     "ClientState",
